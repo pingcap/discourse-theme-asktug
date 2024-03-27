ac90af656e801a5239b1a555a370071b5a38ef31
Jagger <634750802@qq.com>
2024-03-27 15:11:29 +0800

    update

--- javascripts/discourse/initializers/discourse-theme-asktug.js
+++ javascripts/discourse/initializers/discourse-theme-asktug.js
@@ -5,6 +5,7 @@ export default {
   name: 'discourse-theme-asktug',
   initialize() {
     runAll([
+      { name: 'commonHeaderFooter', initializer: commonHeaderFooter },
       { name: 'hideSiteHeader', initializer: hideSiteHeader },
       { name: 'replaceSearchFunction', initializer: replaceSearchFunction },
     ])
@@ -22,7 +23,23 @@ function runAll(funcs) {
   })
 }
 
-function hideSiteHeader () {
+function commonHeaderFooter() {
+  const settings = Discourse.lookup('site-settings:main')
+
+  var BASE = 'https://tidb-community-site.oss-cn-beijing.aliyuncs.com/tidb-community-ui'
+  var TS = settings.community_ui_ts ? '?v=' + settings.community_ui_ts : ''
+  var link = document.createElement('link')
+  link.rel = 'stylesheet'
+  link.href = BASE + '/ti-site-asktug.' + settings.deployment_env + '.css' + TS
+  document.head.appendChild(link)
+
+  var script = document.createElement('script')
+  script.src = BASE + '/ti-site-asktug.' + settings.deployment_env + '.js' + TS
+  script.defer = true
+  document.body.appendChild(script)
+}
+
+function hideSiteHeader() {
   Discourse.lookup('controller:application').reopen({
     @computed()
     showSiteHeader() {
