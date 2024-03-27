8f2f53a52e518ddcdfe2de314827dcb5da257d60
Jagger <634750802@qq.com>
2024-03-27 14:31:54 +0800

    update

--- javascripts/discourse/initializers/discourse-theme-asktug.js
+++ javascripts/discourse/initializers/discourse-theme-asktug.js
@@ -3,29 +3,25 @@ import SearchMenu from 'discourse/components/search-menu';
 export default {
   name: 'discourse-theme-asktug',
   initialize() {
-    console.log('hi', SearchMenu)
+    replaceSearchFunction();
   }
 };
 
-/**
- *
- *
- * const SearchMenu = require('discourse/components/search-menu');
- *
- * function search () {
- *   var term = document.querySelector(`#search-term`).value
- *   window.open('https://search.asktug.com/?q=' + encodeURIComponent(term), '_blank');
- * }
- *
- * // redirect searches
- * var originalTriggerSearch = SearchMenu.prototype.triggerSearch;
- *
- * SearchMenu.prototype.fullSearch = search;
- * SearchMenu.prototype.triggerSearch = function () {
- *   if (event.which === 13) {
- *     search ()
- *   } else {
- *     originalTriggerSearch.call(this)
- *   }
- * }
- */
+function replaceSearchFunction() {
+  function search() {
+    var term = document.querySelector(`#search-term`).value
+    window.open('https://search.asktug.com/?q=' + encodeURIComponent(term), '_blank');
+  }
+
+  // redirect searches
+  var originalTriggerSearch = SearchMenu.prototype.triggerSearch;
+
+  SearchMenu.prototype.fullSearch = search;
+  SearchMenu.prototype.triggerSearch = function () {
+    if (event.which === 13) {
+      search()
+    } else {
+      originalTriggerSearch.call(this)
+    }
+  }
+}
