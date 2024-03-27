abf816ea7777615e96c81f2d823822f103973f3d
Jagger <634750802@qq.com>
2024-03-27 14:25:34 +0800

    update

--- javascripts/discourse/initializers/discourse-theme-asktug.js
+++ javascripts/discourse/initializers/discourse-theme-asktug.js
@@ -1,6 +1,31 @@
+import SearchMenu from 'discourse/components/search-menu';
+
 export default {
   name: 'discourse-theme-asktug',
-  initialize(api) {
-    console.log('hi', arguments)
+  initialize() {
+    console.log('hi', SearchMenu)
   }
 };
+
+/**
+ *
+ *
+ * const SearchMenu = require('discourse/components/search-menu');
+ *
+ * function search () {
+ *   var term = document.querySelector(`#search-term`).value
+ *   window.open('https://search.asktug.com/?q=' + encodeURIComponent(term), '_blank');
+ * }
+ *
+ * // redirect searches
+ * var originalTriggerSearch = SearchMenu.prototype.triggerSearch;
+ *
+ * SearchMenu.prototype.fullSearch = search;
+ * SearchMenu.prototype.triggerSearch = function () {
+ *   if (event.which === 13) {
+ *     search ()
+ *   } else {
+ *     originalTriggerSearch.call(this)
+ *   }
+ * }
+ */
